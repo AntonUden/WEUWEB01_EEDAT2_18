c645f5204f4e64ea7c1803d416151c65c5f13eb7
Anton Udén <udenanton@hotmail.com>
2019-12-02 13:35:53 +0100

    update

---
+++ Uppgift 24/navbar.php
@@ -0,0 +1,13 @@
+<ul class="navbar">
+    <li class="nav-item">
+        <a class="nav-link" href="/Uppgift%2024/">Start</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="/Uppgift%2024/projects.php">Projekt</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="/Uppgift%2024/links.php">Länkar</a>
+    </li>
+</ul>
