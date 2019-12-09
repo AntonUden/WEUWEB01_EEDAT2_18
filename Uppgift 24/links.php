ce032e59971fa36b89f6818711e3028a962f6d0e
Anton Udén <udenanton@hotmail.com>
2019-12-09 13:25:25 +0100

    Added links

--- Uppgift 24/links.php
+++ Uppgift 24/links.php
@@ -1,13 +1,62 @@
 <html>
     <head>
         <link rel="stylesheet" href="./global.css">
+        <link rel="stylesheet" href="./links.css">
     </head>
     
     <body>
         <?php require "navbar.php"; ?>
 
         <div class="mx10px my5px">
-            <h1>test</h1>
+            <div class="link-list">
+                <div class="link-box">
+                    <div class="link-icon">
+                        <img class="link-img" src="./img/uwamp.jpg" alt="UwAmp">
+                    </div>
+                    <div class="link-info">
+                        <div class="link-title">
+                            <a href="https://www.uwamp.com/en/">
+                                UwAmp
+                            </a>
+                        </div>
+                        <div class="link-description">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi eos cupiditate, a ea error illum cumque excepturi temporibus iste alias voluptatem, eius blanditiis tempore. Omnis corporis non numquam laboriosam consectetur?
+                        </div>
+                    </div>
+                </div>
+
+                <div class="link-box">
+                    <div class="link-icon">
+                        <img class="link-img" src="./img/PuTTY_icon_128px.png" alt="PuTTY">
+                    </div>
+                    <div class="link-info">
+                        <div class="link-title">
+                            <a href="https://www.putty.org/">
+                                PuTTY
+                            </a>
+                        </div>
+                        <div class="link-description">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi eos cupiditate, a ea error illum cumque excepturi temporibus iste alias voluptatem, eius blanditiis tempore. Omnis corporis non numquam laboriosam consectetur?
+                        </div>
+                    </div>
+                </div>
+
+                <div class="link-box">
+                    <div class="link-icon">
+                        <img class="link-img" src="./img/Visual_Studio_Code_0.10.1_icon.png" alt="Visual Studio Code">
+                    </div>
+                    <div class="link-info">
+                        <div class="link-title">
+                            <a href="https://code.visualstudio.com/">
+                                Visual Studio Code
+                            </a>
+                        </div>
+                        <div class="link-description">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi eos cupiditate, a ea error illum cumque excepturi temporibus iste alias voluptatem, eius blanditiis tempore. Omnis corporis non numquam laboriosam consectetur?
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
 </html>
